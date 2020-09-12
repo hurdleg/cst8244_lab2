7341ee7e47a07f0326ad0be7acfcf7e532413d74
Gerald Hurdle <hurdleg@algonquincollege.com>
2020-09-12 13:07:41 -0400

    Lab 02 :: Finished

--- src/my_first_project.c
+++ src/my_first_project.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
-	puts("Hello World!!!"); /* prints Hello World!!! */
+	puts("Hello World from QNX Neutrino RTOS!!!"); /* prints Hello World!!! */
+
+	puts( "\n@author Gerald Hurdle (hurdleg@algonquincollege.com)" );
+	puts( "I like to walk my dog Leo." );
+	puts( "" );
+
+	printf( "\n" );
+	printf( "My PID is: %d\n", getpid() );
+	printf( "My Parent's PID is: %d\n", getppid() );
+	printf( "\n" );
+
+	puts( "Enter 'q' to quit: ");
+	getchar();
+
 	return EXIT_SUCCESS;
 }
